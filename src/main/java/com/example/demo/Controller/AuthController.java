b31ec7d5774158387dbfb99611f75c9180802714
Chhorn Vibol <your_email@gmail.com>
2026-05-09 09:23:35 +0700

    update

--- src/main/java/com/example/demo/Controller/AuthController.java
+++ src/main/java/com/example/demo/Controller/AuthController.java
@@ -12,30 +12,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("/api/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
     @GetMapping("/login")
-    public String createProduct(@RequestParam String username, @RequestParam String password)
-    {
-        if (username.equals("admin")  && password.equals("1234")) {
-            return "login Success";
-        }else {
+    public String createProduct(@RequestParam String username, @RequestParam String password) {
+        if (username.equals("admin") && password.equals("1234")) {
+            return "login Successfully";
+        } else {
             return "not Valid";
         }
     }
+
     @GetMapping("/allUser")
-    public List<Map<String,Object>> getAlluser()
-    {
-        List<Map<String,Object>> name = new ArrayList<>();
-        Map<String,Object> user1 = new HashMap<>();
+    public List<Map<String, Object>> getAlluser() {
+        List<Map<String, Object>> name = new ArrayList<>();
+        Map<String, Object> user1 = new HashMap<>();
         user1.put("gf", 14);
         user1.put(" max", 23);
         name.add(user1);
         return name;
     }
+
     @PostMapping("/Register")
-    public String create_integer()
-    {
+    public String create_integer() {
         return "hello world";
+    }
+
+    @GetMapping("admin")
+    public List<Map<String, Object>> getAllAdmin() {
+        List<Map<String, Object>> admin = new ArrayList<>();
+        Map<String, Object> admin1 = new HashMap<>();
+        admin1.put("vibol", 12);
+        admin1.put("coca", 17);
+        admin1.put("qerqer", 34);
+        admin.add(admin1);
+        return admin;
     }
 }
